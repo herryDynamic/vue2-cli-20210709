ee9a2322e92e6d6e502110b788198d08eaa38f4e
baiam <baiam@dchealth.com>
2021-07-09 10:30:39 +0800

    【提交框架】

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+if(process.env.NODE_ENV === "development"){
+    Vue.use(Vuex) 
+} 
 
-Vue.use(Vuex)
+import user from './modules/user'
+import permission from './modules/permission'
+import money from './modules/money'
+import menu from './modules/menu'
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+    modules: {
+        user,
+        permission,
+        money,
+        menu
+    }
+});
+
---
+++ src/views/share/components/index.js
@@ -0,0 +1,20 @@
+import HengShare from "./hengShare.vue";
+import InviteShare from "./inviteShare.vue";
+import JianshuShare from "./jianshuShare.vue";
+import JianshuLeftShare from "./jianshuLeftShare.vue";
+import WxCodeModal from "./wxCodeModal.vue";
+import JuejinShare from "./juejinShare.vue";
+import InfoShare from "./infoShare.vue";
+import SinaShare from "./sinaShare.vue";
+import YanShare from "./yanShare.vue";
+export {
+  HengShare,
+  InviteShare,
+  JianshuShare,
+  JianshuLeftShare,
+  WxCodeModal,
+  JuejinShare,
+  InfoShare,
+  SinaShare,
+  YanShare
+};
